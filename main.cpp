af4692b94a4acaac59369792904923bce1bff716
u211-14 <u211-14@public.mpei.local>
2019-04-02 14:06:12 +0300

    code vvod

--- main.cpp
+++ main.cpp
@@ -2,19 +2,21 @@
 #include <vector>
 
 using namespace std;
-
+vector<double>
+input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
+    }
+    return result;
+}
 int
 main() {
     // ¬вод данных
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
 
-    cerr << "Enter numbers: ";
-    vector<double> numbers(number_count);
-    for (size_t i = 0; i < number_count; i++) {
-        cin >> numbers[i];
-    }
+size_t number_count;
+cin >> number_count;
+const vector<double> numbers = input_numbers(number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
