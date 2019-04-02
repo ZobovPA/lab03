01b5638383c2077593c24827a4d3a502c6152acf
u211-14 <u211-14@public.mpei.local>
2019-04-02 17:12:01 +0300

    part 2

--- main.cpp
+++ main.cpp
@@ -72,6 +72,48 @@ void show_histogram_text(vector<size_t> bins){
     }
 
 }
+
+
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg width='" << width << "' height='" << height << "' "
+         << "viewBox='0 0 " << width << " " << height << "' "
+         << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text){
+cout <<"<text x='" << left <<"' y='"<<baseline <<" '>"<<text<<"</text>";
+}
+void svg_rect(double x, double y, double width, double height,
+        string stroke, string fill){
+cout <<"<rect x='" << x <<"' y='"<< y <<"' width='"<< width <<"' height='"<< height <<"' stroke='"<< stroke <<"' fill='"<< fill <<"'>";
+}
+void
+show_histogram_svg(const vector<size_t>& bins) {
+
+const auto IMAGE_WIDTH = 400;
+const auto IMAGE_HEIGHT = 300;
+const auto TEXT_LEFT = 20;
+const auto TEXT_BASELINE = 20;
+const auto TEXT_WIDTH = 50;
+const auto BIN_HEIGHT = 30;
+string fill = "yellow";
+svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT );
+double top = 0;
+for (size_t bin : bins) {
+    const double bin_width = 10 * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,  "red",  fill);
+    top += BIN_HEIGHT;
+}
+svg_end();
+}
 int
 main() {
     // Ввод данных
@@ -89,11 +131,12 @@ vector<double> numbers = input_numbers(number_count);
 find_minmax(numbers, min, max);
 vector<size_t> bins(bin_count);
  bins = make_histogram(numbers, bin_count);
-show_histogram_text(bins);
+
+show_histogram_svg(bins);
 
 
 
-    // Вывод данных
+
 
 
 
